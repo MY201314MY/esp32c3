8a03d9859988f5d3661e892b2b75ced4613803cf
mteaching <3477669308@qq.com>
2024-11-21 14:29:28 +0800

    gagbuge

--- drivers/modem/tc_modem.c
+++ drivers/modem/tc_modem.c
@@ -1,44 +1,39 @@
-/*Copyright (C) Telit Communications S.p.A. Italy - All Rights Reserved.*/
-/*    See LICENSE file in the project root for full license information.     */
-
-/* Include files ================================================================================*/
-
 #include <drivers/modem/tc_modem.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER( cellular, LOG_LEVEL_DBG );
 
-/* Local function prototypes ====================================================================*/
-static int modem_cellular_init( const struct device *dev );
+LOG_MODULE_REGISTER(cellular, LOG_LEVEL_DBG);
 
-static const struct device *modem = DEVICE_DT_GET( DT_ALIAS( modem ) );
+static int modem_cellular_init(const struct device *dev);
+
+static const struct device *modem = DEVICE_DT_GET(DT_ALIAS( modem ));
 
 static void cb_handler_rx(const struct device *dev)
 {
-  struct tc_modem_data *data = ( struct tc_modem_data * )modem->data;
-  struct tc_modem_config *config = ( struct tc_modem_config * )modem->config;
-	uint8_t rxbuffer[32] = {0};
+    struct tc_modem_data *data = ( struct tc_modem_data * )modem->data;
+    struct tc_modem_config *config = ( struct tc_modem_config * )modem->config;
+    uint8_t rxbuffer[32] = {0};
 
-	int size = uart_fifo_read(dev, rxbuffer, sizeof(rxbuffer));
+    int size = uart_fifo_read(dev, rxbuffer, sizeof(rxbuffer));
 	if(size<=0)
 	{
-		LOG_ERR("failed to read uart: %d", size);
-		return;
+	    LOG_ERR("failed to read uart: %d", size);
+	    return;
 	}
 
-  int cnt = ring_buf_put(&data->rx_rb, rxbuffer, size);
+    int cnt = ring_buf_put(&data->rx_rb, rxbuffer, size);
 
-  if( cnt != size )
-  {
-    LOG_WRN( "modem receive ring buffer overrun!" );
-  }
+    if( cnt != size )
+    {
+        LOG_WRN( "modem receive ring buffer overrun!" );
+    }
 
-	LOG_HEXDUMP_DBG(rxbuffer, size, "RX");
+    LOG_HEXDUMP_DBG(rxbuffer, size, "RX");
 }
 
 static void cb_handler_tx(const struct device *dev)
 {
-  struct tc_modem_data *data = ( struct tc_modem_data * )modem->data;
-  struct tc_modem_config *config = ( struct tc_modem_config * )modem->config;
+    struct tc_modem_data *data = ( struct tc_modem_data * )modem->data;
+    struct tc_modem_config *config = ( struct tc_modem_config * )modem->config;
 	uint8_t txbuffer[32] = {0};
 
 	uint32_t cnt = ring_buf_get(&data->tx_rb, txbuffer, sizeof(txbuffer));
@@ -56,7 +51,6 @@ static void cb_handler_tx(const struct device *dev)
 static void uart_cb_handler(const struct device *dev, void *user_data)
 {
 	if (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
-
 		if (uart_irq_rx_ready(dev)) {
 			cb_handler_rx(dev);
 		}
@@ -67,82 +61,60 @@ static void uart_cb_handler(const struct device *dev, void *user_data)
 	}
 }
 
-static void modem_cellular_transparent_handler( struct k_work *item )
-{
-  struct tc_modem_data *data =
-    CONTAINER_OF( item, struct tc_modem_data, transparent.work );
-
-  if( data->transparent.callback != NULL )
-  {
-    data->transparent.callback( &data->transparent.ring);
-  }
-}
-
 static int modem_cellular_init( const struct device *dev )
 {
-  struct tc_modem_data *data = ( struct tc_modem_data * )dev->data;
-  struct tc_modem_config *config = ( struct tc_modem_config * )dev->config;
-  data->dev = dev;
-  k_mutex_init( &data->rx_mutex );
-  k_mutex_init( &data->tx_mutex );
+    struct tc_modem_data *data = ( struct tc_modem_data * )dev->data;
+    struct tc_modem_config *config = ( struct tc_modem_config * )dev->config;
 
-  LOG_INF("addr: %p", config->uart);
+    k_mutex_init( &data->rx_mutex );
+    k_mutex_init( &data->tx_mutex );
 
-  LOG_DBG( "modem rx buffer size : %d bytes", ARRAY_SIZE( data->rxbuffer ) );
-  LOG_DBG( "modem tx buffer size : %d bytes", ARRAY_SIZE( data->txbuffer ) );
-  {
-    ring_buf_init( &data->rx_rb, sizeof( data->rxbuffer ),
-                   data->rxbuffer );
+    LOG_DBG( "modem rx buffer size : %d bytes", ARRAY_SIZE( data->rxbuffer ) );
+    LOG_DBG( "modem tx buffer size : %d bytes", ARRAY_SIZE( data->txbuffer ) );
+    {
+    ring_buf_init( &data->rx_rb, sizeof( data->rxbuffer ), data->rxbuffer );
 
-    ring_buf_init( &data->tx_rb, sizeof( data->txbuffer ),
-                   data->txbuffer );
-    
-    ring_buf_init( &data->transparent.ring, sizeof( data->transparent.rxbuffer ),
-                   data->transparent.rxbuffer );
+    ring_buf_init( &data->tx_rb, sizeof( data->txbuffer ), data->txbuffer );
+    }
 
-    //k_work_init( &data->transparent.work, modem_cellular_transparent_handler );
-  }
-
-  uart_irq_callback_user_data_set(config->uart, uart_cb_handler, NULL);
-  uart_irq_rx_enable(config->uart);
-	uart_irq_tx_disable(config->uart);
+    uart_irq_callback_user_data_set(config->uart, uart_cb_handler, NULL);
+    uart_irq_rx_enable(config->uart);
+    uart_irq_tx_disable(config->uart);
 
   return 0;
 }
 
-int tc_modem_transparent_transmit( uint8_t *txbuffer, size_t size )
+int tc_transmit( uint8_t *txbuffer, size_t size )
 {
-  struct tc_modem_data *data = ( struct tc_modem_data * )modem->data;
-  struct tc_modem_config *config = ( struct tc_modem_config * )modem->config;
+    struct tc_modem_data *data = ( struct tc_modem_data * )modem->data;
+    struct tc_modem_config *config = ( struct tc_modem_config * )modem->config;
 
-  int ret = ring_buf_put(&data->tx_rb, txbuffer, size);
-	uart_irq_tx_enable(config->uart);
+    int ret = ring_buf_put(&data->tx_rb, txbuffer, size);
+    uart_irq_tx_enable(config->uart);
 
-  LOG_INF("uart : %p", config->uart);
-
-  return ret;
+    return ret;
 }
 
 #define MODEM_CELLULAR_INST_NAME(name, inst) \
-  _CONCAT(_CONCAT(_CONCAT(name, _), DT_DRV_COMPAT), inst)
+    _CONCAT(_CONCAT(_CONCAT(name, _), DT_DRV_COMPAT), inst)
 
 #define MODEM_CELLULAR_DEVICE_TELIT_CELLULAR_MODEM(inst)                                                  \
-  \
-  static struct tc_modem_data MODEM_CELLULAR_INST_NAME(data, inst) = {                 \
+    \
+    static struct tc_modem_data MODEM_CELLULAR_INST_NAME(data, inst) = {                 \
 };                                                                                          \
-  \
-  static struct tc_modem_config MODEM_CELLULAR_INST_NAME(config, inst) = {             \
-              .uart = DEVICE_DT_GET(DT_INST_BUS(inst)),                                          \
-              .power_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, mdm_power_gpios, {}),                 \
-              .extern_power_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, mdm_extern_power_gpios, {}),                 \
-              .power_pulse_duration_ms = 5000,                                                   \
-              .startup_time_ms = 5000,                                                          \
-              .shutdown_time_ms = 5000,                                                          \
+    \
+    static struct tc_modem_config MODEM_CELLULAR_INST_NAME(config, inst) = {             \
+                .uart = DEVICE_DT_GET(DT_INST_BUS(inst)),                                          \
+                .power_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, mdm_power_gpios, {}),                 \
+                .extern_power_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, mdm_extern_power_gpios, {}),                 \
+                .power_pulse_duration_ms = 5000,                                                   \
+                .startup_time_ms = 5000,                                                          \
+                .shutdown_time_ms = 5000,                                                          \
 };                                                                                         \
-  \
-  PM_DEVICE_DT_INST_DEFINE(inst, NULL);                                  \
-  \
-  DEVICE_DT_INST_DEFINE(inst, modem_cellular_init, PM_DEVICE_DT_INST_GET(inst),              \
+    \
+    PM_DEVICE_DT_INST_DEFINE(inst, NULL);                                  \
+    \
+    DEVICE_DT_INST_DEFINE(inst, modem_cellular_init, PM_DEVICE_DT_INST_GET(inst),              \
                         &MODEM_CELLULAR_INST_NAME(data, inst),                               \
                         &MODEM_CELLULAR_INST_NAME(config, inst), POST_KERNEL, 99, NULL);
 
--- include/drivers/modem/tc_modem.h
+++ include/drivers/modem/tc_modem.h
@@ -1,6 +1,3 @@
-/*Copyright (C) Telit Communications S.p.A. Italy - All Rights Reserved.*/
-/*    See LICENSE file in the project root for full license information.     */
-
 #ifndef __INCLUDE_IOTS_DRIVERS_MEX10G1_H__
 #define __INCLUDE_IOTS_DRIVERS_MEX10G1_H__
 
@@ -15,42 +12,26 @@
 #include <zephyr/pm/device.h>
 #include <zephyr/sys/ring_buffer.h>
 
-/* Global declarations ------------------------------------------------------ */
-typedef void ( *modem_transparent_callback )( struct ring_buf *ring );
-/* Global typedefs ---------------------------------------------------------- */
 struct tc_modem_data
 {
-  const struct device *dev;
+    struct k_mutex rx_mutex;
+    struct k_mutex tx_mutex;
 
-  struct k_mutex rx_mutex;
-  struct k_mutex tx_mutex;
+    struct ring_buf rx_rb;
+    uint8_t rxbuffer[512];
+    struct ring_buf tx_rb;
+    uint8_t txbuffer[512];
 
-  /* UART backend */
-  struct ring_buf rx_rb;
-  uint8_t rxbuffer[512];
-  struct ring_buf tx_rb;
-  uint8_t txbuffer[512];
-
-  struct
-  {
-    bool flag;
-    uint8_t rxbuffer[256];
-    struct ring_buf ring;
-    struct k_work work;
-    modem_transparent_callback callback;
-  } transparent;
 };
 
 struct tc_modem_config
 {
-  const struct device *uart;
-  const struct gpio_dt_spec power_gpio;
-  const struct gpio_dt_spec extern_power_gpio;
-  const uint16_t power_pulse_duration_ms;
-  const uint16_t startup_time_ms;
-  const uint16_t shutdown_time_ms;
-};
+    const struct device *uart;
+    const struct gpio_dt_spec power_gpio;
+    const struct gpio_dt_spec extern_power_gpio;
+    const uint16_t power_pulse_duration_ms;
+    const uint16_t startup_time_ms;
+    const uint16_t shutdown_time_ms;
+}
 
-int tc_modem_transparent_transmit( uint8_t *txbuffer, size_t size );
-
-#endif /* __INCLUDE_IOTS_DRIVERS_MEX10G1_H__ */
+#endif
