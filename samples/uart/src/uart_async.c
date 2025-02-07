fef9acbf14370761dc9e5e23df6ae4e3fa9750ee
mteaching <3477669308@qq.com>
2025-02-07 14:19:21 +0800

    ok

--- samples/uart/src/uart_async.c
+++ samples/uart/src/uart_async.c
@@ -10,22 +10,24 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/uart.h>
 
-LOG_MODULE_REGISTER(async, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(async, LOG_LEVEL_INF);
 
-#define BUF_SIZE 2048
+#define BUF_SIZE 512
 static K_MEM_SLAB_DEFINE(uart_slab, BUF_SIZE, 3, 4);
 
-uint8_t txbuffer[2048]="123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
-static void uart_async_callback(const struct device *dev,
-			  struct uart_event *evt,
-			  void *user_data)
+static uint8_t uart_rxbuffer[2048] = {0};
+static struct ring_buf uart_rx_rb;
+
+uint8_t txbuffer[2048]="9012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+
+static void uart_async_callback(const struct device *dev, struct uart_event *evt, void *user_data)
 {
 	struct device *uart = user_data;
-	int err;
+	int ret;
 
 	switch (evt->type) {
 	case UART_TX_DONE:
-		LOG_INF("Tx sent %d bytes", evt->data.tx.len);
+		LOG_DBG("Tx sent %d bytes", evt->data.tx.len);
 		break;
 
 	case UART_TX_ABORTED:
@@ -35,17 +37,23 @@ static void uart_async_callback(const struct device *dev,
 	case UART_RX_RDY:
 		LOG_INF("Received data %d bytes", evt->data.rx.len);
 		LOG_HEXDUMP_DBG(evt->data.rx.buf + evt->data.rx.offset, evt->data.rx.len, "recv");
+		
+		if(ring_buf_put(&uart_rx_rb, evt->data.rx.buf + evt->data.rx.offset, evt->data.rx.len) != evt->data.rx.len)
+		{
+			LOG_WRN("ring buffer overflow.");
+		}
+		
 		break;
 
 	case UART_RX_BUF_REQUEST:
 	{
 		uint8_t *buf;
 
-		err = k_mem_slab_alloc(&uart_slab, (void **)&buf, K_NO_WAIT);
-		__ASSERT(err == 0, "Failed to allocate slab");
+		ret = k_mem_slab_alloc(&uart_slab, (void **)&buf, K_NO_WAIT);
+		__ASSERT(ret == 0, "Failed to allocate slab");
 
-		err = uart_rx_buf_rsp(uart, buf, BUF_SIZE);
-		__ASSERT(err == 0, "Failed to provide new buffer");
+		ret = uart_rx_buf_rsp(uart, buf, BUF_SIZE);
+		__ASSERT(ret == 0, "Failed to provide new buffer");
 		break;
 	}
 
@@ -60,56 +68,102 @@ static void uart_async_callback(const struct device *dev,
 		break;
 	}
 }
-#define ESP32_UART_TRANSMIT_LENGTH_MAX 128 /* DON'T CHANGE. */
+#define UART_ASYNC_TRANSMIT_LENGTH_MAX 64 /* DON'T CHANGE. */
 
-static void async(const struct device *lpuart)
+static int uart_async_transmit(const struct device *dev, uint8_t *buffer, size_t size)
 {
-	int err;
-	uint8_t *buf;
-    uint32_t offset = 0;
-    uint32_t length = 0;
-    uint32_t remain = strlen(txbuffer);
+	int ret;
+    size_t offset = 0;
+    size_t length = 0;
 
-
-	err = k_mem_slab_alloc(&uart_slab, (void **)&buf, K_NO_WAIT);
-	LOG_DBG("L:%d -- error:%d\r\n", __LINE__, err);
-
-	err = uart_callback_set(lpuart, uart_async_callback, (void *)lpuart);
-	LOG_DBG("L:%d -- error:%d\r\n", __LINE__, err);
-
-	err = uart_rx_enable(lpuart, buf, BUF_SIZE, 10000);
-	LOG_DBG("L:%d -- error:%d\r\n", __LINE__, err);
-
-	while (1) {
-        offset = 0;
-		for(;;)
-        {
-            if(remain - offset>=ESP32_UART_TRANSMIT_LENGTH_MAX)
-            {
-                length = ESP32_UART_TRANSMIT_LENGTH_MAX;
-            }else{
-                length = remain-offset;
-            }
-            LOG_INF("tx length : %d", length);
-            err = uart_tx(lpuart, txbuffer+offset, length, 10000);
-            LOG_DBG("L:%d -- error:%d\r\n", __LINE__, err);
-
-            offset += length;
-            if(offset>=remain)
-            {
-                break;
-            }
-        }
-
-        k_sleep(K_MSEC(5000));
+	if(dev == NULL || buffer == NULL)
+	{
+		return -EINVAL;
 	}
+
+	for(;;)
+	{
+		if(size - offset >= UART_ASYNC_TRANSMIT_LENGTH_MAX)
+		{
+			length = UART_ASYNC_TRANSMIT_LENGTH_MAX;
+		}else{
+			length = size-offset;
+		}
+
+		if(length>0)
+		{
+			LOG_INF("tx length : %d", length);
+			ret = uart_tx(dev, txbuffer+offset, length, 10000);
+			LOG_DBG("L:%d -- error:%d\r\n", __LINE__, ret);
+
+			offset += length;
+			if(offset>=size)
+			{
+				break;
+			}
+		}else{
+			break;
+		}
+	}
+
+	return offset;
 }
+
+uint8_t templete[2048] = {0};
 
 int uart_async_init(void)
 {
+	uint32_t index = 0;
+	uint8_t *buf;
 	const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
 
-	async(uart);	
+	ring_buf_init(&uart_rx_rb, sizeof(uart_rxbuffer), uart_rxbuffer);
+	
+	int ret = k_mem_slab_alloc(&uart_slab, (void **)&buf, K_NO_WAIT);
+	if(ret)
+	{
+		LOG_DBG("L:%d -- error:%d\r\n", __LINE__, ret);
+	}
 
+	ret = uart_callback_set(uart, uart_async_callback, (void *)uart);
+	if(ret)
+	{
+		LOG_DBG("L:%d -- error:%d\r\n", __LINE__, ret);
+	}
+
+	ret = uart_rx_enable(uart, buf, BUF_SIZE, 10000);
+	if(ret)
+	{
+		LOG_DBG("L:%d -- error:%d\r\n", __LINE__, ret);
+	}
+	k_sleep(K_SECONDS(3));
+	LOG_INF("ASYNC TX-RX TEST.");
+	while(1)
+	{
+		LOG_INF("test index : %d", index++);
+		uart_async_transmit(uart, txbuffer, strlen(txbuffer));
+		uint32_t count = ring_buf_get(&uart_rx_rb, templete, sizeof(templete));
+
+		LOG_INF("count : %d", count);
+
+		if(count!=strlen(txbuffer))
+		{
+			LOG_ERR("size not the same.");
+			break;
+		}
+
+		if(strcmp(templete, txbuffer) != 0)
+		{
+			LOG_ERR("content not the same.");
+
+			LOG_HEXDUMP_DBG(txbuffer, strlen(txbuffer), "TX");
+
+			LOG_HEXDUMP_DBG(templete, strlen(templete), "RX");
+
+			break;
+		}
+
+		k_sleep(K_SECONDS(5));
+	}
 	return 0;
 }
