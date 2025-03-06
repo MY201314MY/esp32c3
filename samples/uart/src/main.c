804299e13249cc74da1f9e17ccec0b5de18d0565
mteaching <3477669308@qq.com>
2025-03-06 14:29:25 +0800

    test gpio

--- samples/uart/src/main.c
+++ samples/uart/src/main.c
@@ -9,15 +9,43 @@
 #include <zephyr/sys/reboot.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/gpio.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
+#define LED0 DT_ALIAS(led_3)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0,gpios);
 
 int main(void)
 {
+	k_sleep(K_SECONDS(10));
+
+	bool led_state = true;
 	
 	LOG_INF("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+
+		led_state = !led_state;
+		LOG_INF("LED state: %s\n", led_state ? "ON" : "OFF");
+		k_sleep(K_SECONDS(5));
+	}
+
 	
 	return 0;
 }
