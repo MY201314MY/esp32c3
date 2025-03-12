82ec2153954fc81e6db049593b1e9df3fc19e76b
mteaching <3477669308@qq.com>
2025-03-12 15:59:01 +0800

    watch dog.

--- samples/uart/src/main.c
+++ samples/uart/src/main.c
@@ -5,21 +5,90 @@
  */
 
 #include <stdio.h>
+#include <zephyr/device.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/reboot.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/watchdog.h>
 #include <stdlib.h>
+
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
+
+static const struct device *const wdt = DEVICE_DT_GET(DT_ALIAS(watchdog0));
+static int wdt_channel_id;
+#define WDT_MAX_WINDOW  8000U
+#define WDT_MIN_WINDOW  0U
+
+
+static void wdt_callback(const struct device *wdt_dev, int channel_id)
+{
+	printk("watchdog channel %d, ready to reset...\n", channel_id);
+}
+
+int tc_watch_dog_init(void)
+{
+	int ret;
+
+	if (!device_is_ready(wdt)) {
+		printk("%s: device not ready.\n", wdt->name);
+		return -ENODEV;
+	}
+
+	struct wdt_timeout_cfg wdt_config = {
+		/* Reset SoC when watchdog timer expires. */
+		.flags = WDT_FLAG_RESET_SOC,
+
+		/* Expire watchdog after max window */
+		.window.min = WDT_MIN_WINDOW,
+		.window.max = WDT_MAX_WINDOW,
+	};
+
+	/* Set up watchdog callback. */
+	wdt_config.callback = wdt_callback;
+
+	wdt_channel_id = wdt_install_timeout(wdt, &wdt_config);
+	if (wdt_channel_id == -ENOTSUP) {
+		/* IWDG driver for STM32 doesn't support callback */
+		printk("Callback support rejected, continuing anyway\n");
+		wdt_config.callback = NULL;
+		wdt_channel_id = wdt_install_timeout(wdt, &wdt_config);
+	}
+	if (wdt_channel_id < 0) {
+		printk("Watchdog install error\n");
+		return 0;
+	}
+
+	ret = wdt_setup(wdt, WDT_OPT_PAUSE_HALTED_BY_DBG);
+	if (ret < 0) {
+		printk("Watchdog setup error\n");
+		return 0;
+	}
+
+	return 0;
+}
+
+void tc_watch_dog_feed()
+{
+	wdt_feed(wdt, wdt_channel_id);
+}
+
 int main(void)
 {
 	k_sleep(K_SECONDS(2));
+	tc_watch_dog_init();
 	
 	LOG_INF("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	while(1)
+	{
+		tc_watch_dog_feed();
+		k_sleep(K_MSEC(1000));
+	}
 
 	return 0;
 }
